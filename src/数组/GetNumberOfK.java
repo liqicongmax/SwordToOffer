fbc05da9ac9839110fda9e896e4ff6445d305ac0
liqicongmax <952267787@qq.com>
2019-07-18 18:57:56 +0800

    剑指offer:难度中等:非递归,二叉树的层序遍历并保存

---
+++ src/数组/GetNumberOfK.java
@@ -0,0 +1,18 @@
+package 数组;
+
+/**
+ * @author liqicong@myhexin.com
+ * @date 2019/7/18 18:55
+ */
+public class GetNumberOfK {
+    /**
+     * 统计一个数K在排序数组arr中出现的次数
+     * @param arr 排序数组
+     * @param k 给定数字k
+     * @return 出现次数
+     * TODO 层序遍历 环入口,再写3题算法看视频
+     */
+    public static int getNumberOfK(int[] arr,int k){
+
+    }
+}
---
+++ src/树/BreadthFirstTraversal.java
@@ -0,0 +1,49 @@
+package 树;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author liqicong@myhexin.com
+ * @date 2019/7/18 17:05
+ */
+public class BreadthFirstTraversal {
+    /**
+     * 二叉树的层序遍历
+     *
+     * @param root 根节点
+     * @return
+     */
+    public ArrayList<ArrayList<Integer>> Print(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        queue.offer(root);
+        int start=0;
+        int end=1;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            temp.add(node.value);
+            start++;
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+            if(start==end){
+                end=queue.size();
+                start=0;
+                list.add(temp);
+                temp=new ArrayList<>();
+            }
+        }
+
+        return list;
+    }
+}
