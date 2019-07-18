c6cee260c28a755716c282818794313d39e8581b
liqicongmax <952267787@qq.com>
2019-07-18 16:54:37 +0800

    剑指offer:难度简单:字符串流中第一个重复的字符

---
+++ src/字符串/FindFirstOnceAppear.java
@@ -0,0 +1,29 @@
+package 字符串;
+
+/**
+ * @author liqicong@myhexin.com
+ * @date 2019/7/18 16:17
+ */
+public class FindFirstOnceAppear {
+    /**
+     * 请实现一个函数用来找出字符流中第一个只出现一次的字符。
+     * 例如，当从字符流中只读出前两个字符"go"时，第一个只出现一次的字符是"g"。
+     * 当从该字符流中读出前六个字符“google"时，第一个只出现一次的字符是"l"。
+     * @param s
+     * @return
+     */
+    int[] hash=new int[256];
+    StringBuilder s=new StringBuilder("");
+    public void Insert(char ch){
+        s.append(ch);
+        hash[ch]+=1;
+    }
+    public char firstAppearOnce(){
+        for(int i=0;i<s.toString().length();i++){
+            if(hash[s.charAt(i)]==1){
+                return s.charAt(i);
+            }
+        }
+        return '#';
+    }
+}
